b46766a26a4e9acdbeeef11e1a93b17c8d37705d
Dennis-DW <denny012dw@gmail.com>
2024-05-14 12:36:36 +0300

    update2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -56,7 +56,7 @@ function App() {
       // Save user to MongoDB
       if (profileObj) {
         try {
-          const response = await fetch("http://localhost:8080/api/v1/users", {
+          const response = await fetch("https://kejani-homes.onrender.com/api/v1/users", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -139,7 +139,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://kejani-homes.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
